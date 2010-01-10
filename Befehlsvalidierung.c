1969112c3cac9cbdc8b8702cf4158a17152a0f34
vitalivoroth@web.de <vitalivoroth@web.de@6f1b9872-ba2f-11de-92a3-373a1addbe3b>
2010-01-10 19:32:16 +0000

    Befehlsvalidierung-Skelett
    git-svn-id: http://sichereeisenbahnsteuerung.googlecode.com/svn/trunk@8 6f1b9872-ba2f-11de-92a3-373a1addbe3b

---
+++ Befehlsvalidierung.c
@@ -0,0 +1,57 @@
+/*****************************************************************************
+ *
+ *        Dateiname:    Befehlsvalidierung.c
+ *
+ *        Projekt:      Sichere Eisenbahnsteuerung
+ *
+ *        Autor:        Vitali Voroth
+ *
+ *
+ *        Modul:        <Modulname>, <Version des Moduldesigns>
+ *
+ *        Modul:        Befehlsvalidierung, 1.0
+ *
+ *        Beschreibung:	Prüft Streckenbefehle der Leitzentrale auf Gültigkeit
+ *			Leitet Sensordaten vom S88-Treiber an die Leitzentrale
+ *			Macht ein Not-Aus bei kritischem Zustand auf Strecke
+ *
+ ****************************************************************************/
+
+/* Includes *****************************************************************/
+#include "Betriebsmittelverwaltung.h"
+#include "Befehlsvalidierung.h"
+#include <stdio.h>
+
+/* Definition globaler Konstanten *******************************************/
+
+/* Definition globaler Variablen ********************************************/
+
+/* Lokale Makros ************************************************************/
+
+/* Lokale Typen *************************************************************/
+
+/* Lokale Konstanten ********************************************************/
+
+/* Lokale Variablen *********************************************************/
+
+/* Prototypen fuer lokale Funktionen ****************************************/
+
+/* Funktionsimplementierungen ***********************************************/
+
+byte b1, b2, b3;
+
+void initBV(void)
+{
+	b1 = 10;
+	b2 = 20;
+
+
+}
+
+void workBV(void)
+{
+	b3 = b1 + b2;
+	b1++;
+	b2++;
+	printf("b3 = %d \n", (int)b3);
+}
---
+++ Befehlsvalidierung.h
@@ -0,0 +1,54 @@
+#ifndef BEFEHLSVALIDIERUNG_H
+#define BEFEHLSVALIDIERUNG_H
+/*****************************************************************************
+ *
+ *        Dateiname:    Befehlsvalidierung.h
+ *
+ *        Projekt:      Sichere Eisenbahnsteuerung
+ *
+ *        Autor:        Vitali Voroth
+ *
+ *
+ *        Modul:        Befehlsvalidierung, 1.0
+ *
+ *        Beschreibung:	Prüft Streckenbefehle der Leitzentrale auf Gültigkeit
+ *			Leitet Sensordaten vom S88-Treiber an die Leitzentrale
+ *			Macht ein Not-Aus bei kritischem Zustand auf Strecke
+ *			Die Header-Datei definiert die Struktur Gleisabschnitt,
+ *			die von der Leitzentrale verwendet wird.
+ *
+ ****************************************************************************/
+
+
+/* Globale Makrodefinitionen ************************************************/
+
+/* Globale Typdefinitionen **************************************************/
+typedef struct Gleisabschnitt_struct 
+{ 
+	byte nr;
+	byte next1;
+	byte next2;
+	byte prev1;
+	byte prev2;
+	byte nextSwitch;
+	byte prevSwitch;
+	byte nextSensor;
+	byte prevSensor;
+
+} Gleisabschnitt;
+
+/* Deklaration von globalen Konstanten **************************************/
+
+/* Deklaration von globalen Variablen ***************************************/
+
+Gleisabschnitt BV_streckentopologie[];
+byte BV_gleisBelegung[];
+byte BV_weichenBelegung[];
+byte BV_zugPosition[];
+
+/* Deklaration von globalen Funktionen **************************************/
+
+void initBV();
+void workBV();
+
+#endif /* BEFEHLSVALIDIERUNG_H */
