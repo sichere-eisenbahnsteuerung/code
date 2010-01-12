e5a49e1ca1c64636da615524859c8d00821572b2
altan@stud.hs-bremen.de <altan@stud.hs-bremen.de@6f1b9872-ba2f-11de-92a3-373a1addbe3b>
2010-01-12 14:42:08 +0000

    Skelett von Vitali durch meine ersetzt
    git-svn-id: http://sichereeisenbahnsteuerung.googlecode.com/svn/trunk@17 6f1b9872-ba2f-11de-92a3-373a1addbe3b

--- Betriebsmittelverwaltung.c
+++ Betriebsmittelverwaltung.c
@@ -1,23 +1,91 @@
-#include <reg515C.h>				/* define 8051 registers */
+/*****************************************************************************
+ *
+ *        Dateiname:    Betriebsmittelverwaltung.c
+ *
+ *        Projekt:      Sichere Eisenbahnsteuerung
+ *
+ *        Autor:        Altan Gottwald
+ *
+ *
+ *        Modul:        Betriebsmittelverwaltung, ver 0.3
+ *
+ *        Beschreibung:
+ *        Init aller Module
+ *        Aufruf aller Module
+ *        ________________________________________________________________
+ *        ________________________________________________________________
+ *        ________________________________________________________________
+ *        ________________________________________________________________
+ *
+ ****************************************************************************/
+
+/* Includes *****************************************************************/
+#include <reg515c.h>
 #include <stdio.h>
 #include "Betriebsmittelverwaltung.h"
-#include "Befehlsvalidierung.h"
+/*
+#include <Notaus.c>
+#include <AuditingSystem>
+#include <RS232>
+#include <S88>
+#include <SSC>
+#include <Ergebnisvalidierung>
+#include <Befehlsverwaltung>
+#include <Leitzentrale>
+#include <Fahrprogramm>
+*/
 
-void main(void) {
-	byte z;	
-
-	SCON = 0x52;	/* SCON */		/* setup serial port control */
-	TMOD = 0x20;	/* TMOD */		/* hardware (2400 BAUD @12MHZ) */
-	TCON = 0x69;	/* TCON */
-	TH1 =  0xf3;	/* TH1 */
-
-	printf ("\n\nC compiler demonstration program\n\n");
-
-	initBV();
-
-	for (z=1; z < 5; z++)  {
-		workBV();
+/* Funktionsimplementierungen ***********************************************/
+void main (void)
+{
+	EAL = 1; //enable all interrupts
+	//alles initialisieren
+/*	
+	initNOTAUS();
+	void initSW ();
+	void initAS (void);
+	void hello ();
+	void initRS232 (void); 
+	void hello ();
+	void initS88 (void);
+	void hello (); 
+	void initSSC (void);
+	void hello ();
+	void initEV (void);
+	void hello ();
+	void initBV (void);
+	void hello ();
+	void initLZ (void);
+	void hello ();
+	void initFP (void);
+	void hello ();
+*/
+	
+	for(;;)	//Endlosschleife der aufrufe, s. "C-Programmierung"
+	{
+		void workLZ (void);		//modulaufruf
+		void hello (void);		//beim SW rückmelden
+		void workRS232 (void);
+		void hello (void);
+		void workS88 (void);
+		void hello (void);
+		void workSSC (void);
+		void hello (void);
+		void workBV (void);
+		void hello (void);
+		void workAS (void);
+		void hello (void);
+		void workRS232 (void);
+		void hello (void);
+		void workSSC (void);
+		void hello (void);
+		void workRS232 (void);
+		void hello (void);
+		void workEV (void);
+		void hello (void);
+		void workSSC (void);
+		void hello (void);
+		void workAS (void);
+		void hello (void);
 	}
-
-	while(TRUE); // Endlosschleife
 }
--- Betriebsmittelverwaltung.h
+++ Betriebsmittelverwaltung.h
@@ -1,11 +1,38 @@
 #ifndef BETRIEBSMITTELVERWALTUNG_H
 #define BETRIEBSMITTELVERWALTUNG_H
+/*****************************************************************************
+ *
+ *        Dateiname:    Betriebsmittelverwaltung.h
+ *
+ *        Projekt:      Sichere Eisenbahnsteuerung
+ *
+ *        Autor:        Altan Gottwald
+ *
+ *
+ *        Modul:        Betriebsmittelverwaltung, ver 0.4
+ *
+ *        Beschreibung:
+ *        Belegung von Ressourcen
+ *        unsigned char sind als typedef bye definiert
+ *        Timer0 wird vom SW verwendet
+ *        ________________________________________________________________
+ *        ________________________________________________________________
+ *        ________________________________________________________________
+ *
+ ****************************************************************************/
+/* Globale Makrodefinitionen ************************************************/
+/**
+    #define <NAME> <ersetzung>
+    dies ersetzt im gesamten quelltext begriffe
+**/
 
 #define TRUE 1 
 #define FALSE 0 
 #define LEER 255
 
+/* Globale Typdefinitionen **************************************************/
 typedef unsigned char byte;
+typedef bit boolean;	
 
 typedef struct 
 { 
@@ -22,4 +49,39 @@ typedef struct
 	byte Fehler; 
 } Sensordaten;
 
-#endif /* BETRIEBSMITTELVERWALTUNG_H */
+/* Deklaration von globalen Variablen ***************************************/
+Streckenbefehl LZ_BV_streckenbefehl = {LEER,LEER,LEER,0};
+Sensordaten BV_LZ_sensordaten = {LEER,LEER,0}; 
+byte BV_LZ_bestaetigung = LEER; 
+Sensordaten S88_BV_sensordaten = {LEER,LEER,0};
+Streckenbefehl BV_EV_streckenbefehl = {LEER,LEER,LEER,0};
+Streckenbefehl SCC_EV_streckenbefehl = {LEER,LEER,LEER,0};
+Streckenbefehl EV_SSC_streckenbefehl = {LEER,LEER,LEER,0};
+byte EV_SSC_failure = LEER;
+byte SSC_EV_failure = LEER; 
+byte SW_status_array[6] = {LEER,LEER,LEER,LEER,LEER,LEER}; 
+Streckenbefehl EV_RS232_streckenbefehl = {LEER,LEER,LEER,0};
+//neu
+byte AS_msg_counter = LEER;
+
+/**
+   Interrupt 1 für SW
+   Interrupt 4 für RS232
+   Interrupt 18 für S88
+**/
+
+//EAL = 1; //enable all interrupts
+
+//Pins
+//P4.1 bis P4.4 für SSC reserviert
+//P4.6 und P4.7 für CAN reserviert
+sbit S88_PS = P1^6; 
+sbit S88_RESET = P1^7;     
+sbit S88_CLK = P3^2;
+sbit S88_Data = P3^3;
+sbit NOTAUS_PIN	= P3^4;
+sbit RS232TREIBER_CTSPIN = P3^5;
+sbit AS_PORT_I2C_SDA = P3^6;
+sbit AS_PORT_I2C_SCL = P3^7;
+
+#endif /* <BETRIEBSMITTELVERWALTUNG>_H */
