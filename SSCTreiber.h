f20744dca50f1c9266dd67f345f0f398ab8665c2
mbuss@stud.hs-bremen.de <mbuss@stud.hs-bremen.de@6f1b9872-ba2f-11de-92a3-373a1addbe3b>
2010-01-13 11:15:50 +0000

    Headerdatei erstellt und Codedatei umbenannt
    git-svn-id: http://sichereeisenbahnsteuerung.googlecode.com/svn/trunk@21 6f1b9872-ba2f-11de-92a3-373a1addbe3b

---
+++ SSCTreiber.h
@@ -0,0 +1,36 @@
+#ifndef SSCTreiber_H
+#define SSCTreiber_H
+/*****************************************************************************
+ *
+ *        Dateiname:    SSCTreiber.h
+ *
+ *        Projekt:      Sichere Eisenbahnsteuerung
+ *
+ *        Autor:        Matthias Buﬂ
+ *		  Datum:		13.01.2010
+ *
+ *        Modul:        SSC, 0.7
+ *
+ *        Beschreibung:
+ *        Die Headerdatei definiert die globalen Funktionen
+ *        initSSC() und workSSC() 
+ *
+ ****************************************************************************/
+
+
+
+
+/* Globale Makrodefinitionen ************************************************/
+
+/* Globale Typdefinitionen **************************************************/
+
+/* Deklaration von globalen Konstanten **************************************/
+
+/* Deklaration von globalen Variablen ***************************************/
+
+/* Deklaration von globalen Funktionen **************************************/
+
+void initSSC();
+void workSSC();
+
+#endif /* SSCTreiber_H */
