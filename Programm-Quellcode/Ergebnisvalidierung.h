64b12701a1fa741961b4fe1a3c89a0efb2066e88
felix.geber@gmx.de <felix.geber@gmx.de@6f1b9872-ba2f-11de-92a3-373a1addbe3b>
2010-05-26 15:08:43 +0000

    Hochladen des Modul-Designs der Ergebnisvalidierung, Kommentaränderungen in dessen Quellcode
    git-svn-id: http://sichereeisenbahnsteuerung.googlecode.com/svn/trunk@210 6f1b9872-ba2f-11de-92a3-373a1addbe3b

--- Programm-Quellcode/Ergebnisvalidierung.h
+++ Programm-Quellcode/Ergebnisvalidierung.h
@@ -11,10 +11,10 @@
  *
  *        Modul:        Ergebnisvalidierung, 1.0
  *
- *        Beschreibung:
- *		Das Modul Ergebnisvalidierung dient der berprfung der von 
+ *        Beschreibung:		
+ *		Das Modul Ergebnisvalidierung dient der Ueberpruefung der von 
  *		der Anwendung erzeugten Streckenbefehle auf Korrektheit. 
- *		Hierzu werden diese ber die SSC-Schnittstelle zwischen den 
+ *		Hierzu werden diese ueber die SSC-Schnittstelle zwischen den 
  *		Mikrocontrollern ausgetauscht und verglichen. Unterscheiden 
  *		sich diese voneinander, wird ein Not-Aus-Signal erzeugt und 
  *		eine Statusmeldung mit den abweichenden Werten an das 
