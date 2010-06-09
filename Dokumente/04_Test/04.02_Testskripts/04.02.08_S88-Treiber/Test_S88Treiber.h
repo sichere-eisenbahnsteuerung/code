51ea9283197f8021d2978a966585373b5ebb1566
jicken@stud.hs-bremen.de <jicken@stud.hs-bremen.de@6f1b9872-ba2f-11de-92a3-373a1addbe3b>
2010-06-09 20:21:10 +0000

    Testskripte für den S88_Treiber
    git-svn-id: http://sichereeisenbahnsteuerung.googlecode.com/svn/trunk@223 6f1b9872-ba2f-11de-92a3-373a1addbe3b

---
+++ Dokumente/04_Test/04.02_Testskripts/04.02.08_S88-Treiber/Test_S88Treiber.h
@@ -0,0 +1,32 @@
+#ifndef TEST_S88TREIBER_H
+#define TEST_S88TREIBER_H
+/*****************************************************************************
+ *
+ *        Dateiname:    Test_S88Treiber.h
+ *
+ *        Projekt:      Sichere Eisenbahnsteuerung
+ *
+ *        Autor:        Jan-Christopher Icken
+ *
+ *
+ *        Modul:        Testskript für das S88-Treiber-Modul
+ *
+ *        Beschreibung:
+ *        ________________________________________________________________
+ *        ________________________________________________________________
+ *
+ ****************************************************************************/
+#include "Betriebsmittelverwaltung.h"
+/* Globale Makrodefinitionen ************************************************/
+
+/* Globale Typdefinitionen **************************************************/
+
+
+/* Deklaration von globalen Konstanten **************************************/
+
+/* Deklaration von globalen Variablen ***************************************/
+
+/* Deklaration von globalen Funktionen **************************************/
+void work_s88_tests(void);
+
+#endif /* TEST_S88TREIBER_H */
