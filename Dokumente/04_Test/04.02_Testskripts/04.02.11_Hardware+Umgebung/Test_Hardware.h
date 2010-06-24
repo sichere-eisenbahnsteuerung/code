8e3eb061e93cba3e5b86d63d738b96c9e59e1f88
normniess@msn.com <normniess@msn.com@6f1b9872-ba2f-11de-92a3-373a1addbe3b>
2010-06-24 13:21:19 +0000

    Änderungen an Test_Hardware-Skript,
    ist jedoch nicht vollständig und auch nicht lauffähig (Gründe nicht bekannt)!
    
    git-svn-id: http://sichereeisenbahnsteuerung.googlecode.com/svn/trunk@295 6f1b9872-ba2f-11de-92a3-373a1addbe3b

---
+++ Dokumente/04_Test/04.02_Testskripts/04.02.11_Hardware+Umgebung/Test_Hardware.h
@@ -0,0 +1,60 @@
+#ifndef Test_Hardware_H
+#define Test_Hardware_H
+/*****************************************************************************
+ *
+ *        Dateiname:    Test_Hardware.h
+ *
+ *        Projekt:      Sichere Eisenbahnsteuerung
+ *
+ *        Autor:        Norman Nieß
+ *						Kai Dziembala
+ *
+ *        Modul:        Testspezifikation Hardware+Umgebungskomponenten, 1.0
+ *        Beschreibung:
+ *        Mit einem Mikrocontroller-Programm wird die Lokomotive 1 dauerhaft auf dem Hauptgleis
+ *	  	  vorwärts bewegt. Gleichzeitig werden über den Arduino Debug-Ausgaben getätigt. Somit kann
+ *        sichergestellt werden, dass der Mikrocontroller, die Multimaus, der XpressNet-Adapter, der
+ *        DCC-Verstärker, die entsprechenden Transformatoren, die Lokomotive 1, der Arduino und die
+ *        jeweilige Verkabelung funktionstüchtig sind.
+ *        Dieser Test muss sowohl mit dem Mikrocontroller eins und zwei durchgeführt werden.
+ *
+ ****************************************************************************/
+
+#include <reg515c.h>
+/* Globale Makrodefinitionen ************************************************/
+#ifndef MODUL_LZ
+#define MODUL_LZ 0
+#endif
+
+#ifndef MODUL_BV
+#define MODUL_BV 1
+#endif
+
+#ifndef MODUL_EV
+#define MODUL_EV 2
+#endif	
+
+/* Globale Typdefinitionen **************************************************/
+
+/* Deklaration von globalen Konstanten **************************************/
+#define V_STOPP 0x00
+#define V_Abkuppeln 0x08
+#define V_Ankuppeln 0x1F
+#define V_Fahrt 0xDD
+#define Lok1_address 0x01
+#define Lok2_address 0x02
+#define W1_address 0x03
+#define W2_address 0x04
+#define W3_address 0x02
+#define EK1_address 0x06
+#define EK2_address 0x05
+#define CTS_readyToSend 1
+#define CTS_stop_send 0
+
+/* Deklaration von globalen Variablen ***************************************/
+sbit RS232TREIBER_CTSPIN = P3^5;
+
+/* Deklaration von globalen Funktionen **************************************/
+void main(void);
+
+#endif /* Test_Hardware_H */
---
+++ Dokumente/04_Test/04.02_Testskripts/04.02.11_Hardware+Umgebung/Test_Sensoren.c
@@ -0,0 +1,101 @@
+/*****************************************************************************
+ *
+ *        Dateiname:    Test_Sensoren.c
+ *
+ *        Projekt:      Sichere Eisenbahnsteuerung
+ *
+ *        Autor:        Norman Nieß
+ *						Kai Dziembala
+ *
+ *        Modul:        Testspezifikation Hardware+Umgebungskomponenten, 1.0
+ *
+ *        Beschreibung:
+ *        Die beiden Lokomotiven werden mit angekoppelten Wagons nacheinander
+ *        Multi-Maus-ferngesteuert, jeweils mit langsamer, mittlerer und maximaler
+ *        Geschwindigkeit über das Streckennetz bewegt. Bei diesem manuellen Fahren
+ *        muss sichergestellt sein, das jeder Lokomotiven- und Wagon- Magnet alle
+ *        Hall-Sensoren mindestens einmal passiert und somit schaltet. Zeitgleich
+ *        läuft auf dem Mikrocontroller dauerhaft ein Steuerprogramm für das
+ *        S88-Rückmeldemodul, wodurch die jeweiligen Sensordaten kontinuierlich
+ *        eingelesen werden. Ein Abgriff dieser jeweils vom S88-Rückmeldemodul
+ *        gelieferten Sensorzustände erfolgt über einen angeschlossenen Logikanalysator
+ *        „Agilent Logic Wave“. Dessen Binärmesswerte sind mittels Rechnerkopplung und
+ *        entsprechender Software einsehbar. Somit kann überprüft werden, ob jeder Magnet
+ *        bei den drei verschiedenen Fahrgeschwindigkeiten ein Sensorsignal für jeden
+ *        Hall-Sensor erzeugt und somit die hardwareseitige Sensordatenerfassung
+ *        funktioniert. Dieser Test muss jeweils für das Rückmeldemodul 1 und 2
+ *        durchgeführt werden, wodurch auch beide Sensorsätze auf Funktion überprüft
+ *        werden.
+ *
+ ****************************************************************************/
+
+/* Includes *****************************************************************/
+#include <Test_Sensoren.h>
+#include <intrins.h>
+
+/* Definition globaler Konstanten *******************************************/
+
+/* Definition globaler Variablen ********************************************/
+int i;
+
+/* Lokale Makros ************************************************************/
+#define HIGH 1
+#define LOW 0
+
+/* Lokale Typen *************************************************************/
+
+/* Lokale Konstanten ********************************************************/
+
+/* Lokale Variablen *********************************************************/
+
+/* Prototypen fuer lokale Funktionen ****************************************/
+void initS88(void);
+void getSensorData(void);
+void wait(byte times);
+
+/* Funktionsimplementierungen ***********************************************/
+
+void main (void) {
+	initS88();
+
+	for(;;)	{
+		getSensorData();
+		Testlampe = P1^0;
+	}
+}
+
+void initS88(void) {
+	S88_PS = LOW;
+	S88_RESET = LOW;
+	S88_CLK = LOW;
+}
+
+void getSensorData() {
+	S88_PS = HIGH;
+	wait(3);
+	S88_CLK = HIGH;
+	wait(9);
+	S88_CLK = LOW;
+	wait(9);
+	S88_RESET = HIGH;
+	wait(7);
+	S88_RESET = LOW;
+	wait(7);
+	S88_PS = LOW;
+	wait(9);
+
+	for(i = 1;i< 16;i++) {
+		S88_CLK = HIGH;
+		wait(9);
+		S88_CLK = LOW;
+		wait(9);
+	}
+}
+
+void wait(byte times) {
+	while(times > 0) 
+	{
+		_nop_();
+		times--;
+	}
+}
